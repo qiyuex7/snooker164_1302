688a52751b63fbcff56cc0e91c24e66d13fd943a
qiyuex7 <cmm81390@gmail.com>
2026-07-18 14:55:20 +0700

    8Ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public enum BallColor
+{
+    White,
+    red,
+    Yellow,
+    Green,
+    Brown,
+    Blue,
+    Pink,
+    Black
+}
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private int point;
+
+    [SerializeField]
+    private BallColor color;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
