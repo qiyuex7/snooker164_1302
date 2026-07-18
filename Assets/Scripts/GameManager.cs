49bbb5850c9a0e4c9047e4d8542215fa872fd526
qiyuex7 <cmm81390@gmail.com>
2026-07-18 15:28:09 +0700

    Event System

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private int playerScore;
+    public int PlayerScore { get { return playerScore; } set { playerScore = value; } }
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
