0d95c7b4e9a7c4b9a26a3b1376d2ac22c57dc632
Bunboont <cmm81390@gmail.com>
2026-08-22 15:41:40 +0700

    Finish SetBall

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,7 +4,7 @@ using UnityEngine.EventSystems;
 public enum BallColor
 {
     White,
-    red,
+    Red,
     Yellow,
     Green,
     Brown,
@@ -21,11 +21,19 @@ public class Ball : MonoBehaviour, IPointerClickHandler
     [SerializeField]
     private BallColor color;
 
+    [SerializeField]
+    private MeshRenderer rd;
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log(point);
         GameManager.instance.PlayerScore += point;
         Destroy(gameObject);
+    }
+
+    void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -39,4 +47,45 @@ public class Ball : MonoBehaviour, IPointerClickHandler
     {
         
     }
+
+    public void SetColorAndPoint(BallColor col)
+    {
+        switch (col)
+        {
+            case BallColor.White:
+                point = 0;
+                rd.material.color = Color.white;
+                break;
+            case BallColor.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+            case BallColor.Yellow:
+                point = 2;
+                rd.material.color = Color.yellow;
+                break;
+            case BallColor.Green:
+                point = 3;
+                rd.material.color = Color.green;
+                break;
+            case BallColor.Brown:
+                point = 4;
+                rd.material.color = Color.brown;
+                break;
+            case BallColor.Blue:
+                point = 5;
+                rd.material.color = Color.blue;
+                break;
+            case BallColor.Pink:
+                point = 6;
+                rd.material.color = Color.pink;
+                break;
+            case BallColor.Black:
+                point = 7;
+                rd.material.color = Color.black;
+                break;
+        }
+    }
+    
+    
 }
