550be9cf93380949203e7099c9ace8bab8ce4f52
qiyuex7 <cmm81390@gmail.com>
2026-08-25 11:38:56 +0700

    Rotate Ball

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,9 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject cueBall;
 
+    [SerializeField]
+    private float xInput = 0f;
+
     public static GameManager instance;
 
     private void Awake()
@@ -38,9 +41,19 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RotateBall();
+
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
             ShootBall();
+
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+            xInput = -0.1f;
+        else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+            xInput = 0.1f;
+        else
+            xInput = 0f;
     }
+
 
     private void SetBall(BallColor col, int i)
     {
@@ -56,5 +69,11 @@ public class GameManager : MonoBehaviour
     {
         Rigidbody rd = cueBall.GetComponent<Rigidbody>();
         rd.AddRelativeForce(Vector3.forward * 50, ForceMode.Impulse);
+    }
+
+    private void RotateBall()
+    {
+        if (cueBall != null)
+            cueBall.transform.Rotate(new Vector3(0f, xInput, 0f));
     }
 }
