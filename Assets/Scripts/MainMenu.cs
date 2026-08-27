51db6b2a4efa2fa75e53a4fa49a03eecbe4dd483
qiyuex7 <cmm81390@gmail.com>
2026-08-27 15:07:39 +0700

    Adjust Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject adjustPanel;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,5 +33,10 @@ public class MainMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void ShowHideAdjustPanel(bool flag)
+    {
+        adjustPanel.SetActive(flag);
     }
 }
